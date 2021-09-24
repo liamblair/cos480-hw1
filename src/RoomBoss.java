38d9f7fb45124a418e8035bd0aef6d86f3022a99
liamblair <liamblair111@gmail.com>
2021-09-24 17:42:11 -0400

    added db instance, connect works, createTables works

--- src/RoomBoss.java
+++ src/RoomBoss.java
@@ -7,11 +7,12 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class RoomBoss {
 
-    private String url;
+    private String url = "jdbc:sqlite:db.sqlite";
 
     public RoomBoss(){
         // Do I even need this??
@@ -52,7 +53,7 @@ public class RoomBoss {
                 ");";
 
         String facilities = "CREATE TABLE IF NOT EXISTS Facilities (\n" +
-                "id integer PRIMARY KEY" +
+                "id integer PRIMARY KEY,\n" +
                 "building varchar(20) NOT NULL,\n" +
                 "room integer NOT NULL,\n" +
                 "chair integer NOT NULL,\n" +
@@ -61,13 +62,24 @@ public class RoomBoss {
                 ");";
 
         String reservations = "CREATE TABLE IF NOT EXISTS Reservations (\n" +
-                "cnum integer PRIMARY KEY" +
+                "cnum integer PRIMARY KEY,\n" +
                 "rstart text NOT NULL,\n" +
                 "rend text NOT NULL,\n" +
                 "building varchar(20) NOT NULL,\n" +
                 "room integer NOT NULL,\n" +
                 "name text NOT NULL\n" +
                 ");";
+
+        try {
+            Connection conn = DriverManager.getConnection(url);
+            Statement statement = conn.createStatement();
+            statement.execute(rooms);
+            statement.execute(facilities);
+            statement.execute(reservations);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void destroyTables() {
--- src/Runner.java
+++ src/Runner.java
@@ -3,12 +3,14 @@ import java.io.File;
 public class Runner {
     public static void main(String[] args) {
         RoomBoss roomBoss = new RoomBoss();
+        roomBoss.connect("","");
+        roomBoss.createTables();
 
-        if (args[0] == "<") {
-            File file = new File(args[1]);
-
-        } else {
-
-        }
+//        if (args[0] == "<") {
+//            File file = new File(args[1]);
+//
+//        } else {
+//
+//        }
     }
 }
